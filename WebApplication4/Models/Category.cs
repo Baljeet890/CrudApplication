0cd22ab94c2d51fe55b0f00d7e6cbbb6f069deb1
Baljeet <0201ite015@gmail.com>
2023-10-01 23:02:39 +0530

    Add project files.

---
+++ WebApplication4/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100,ErrorMessage ="Please input valid value")]
+          public int  DisplayOrder { get; set; }
+    }
+
+}
